c993b7139543e5f617a3f6fe776e5b534e5862a4
JasperPaul <jasper.paul@zohocorp.com>
2025-05-29 22:48:10 +0530

    Update TokenConstants.cs with placeholder values for client credentials and refresh token

--- dotnet_csharp/TokenConstants.cs
+++ dotnet_csharp/TokenConstants.cs
@@ -8,9 +8,9 @@ namespace CSharpFNF
 {
     public class TokenConstants
     {
-        public static string CLIENT_ID = "1004.HUOYH8DEEL6GG5RKQFVTWYAWPFS7CD";
-        public static string CLIENT_SECRET = "5f631cbc570ebc9abbce968de113fc667d4c779f07";
-        public static string REFRESH_TOKEN = "1004.f1fbe45b482bbbc95f54b2522271e4ab.e2436a72870920df8c94c9fb12d93bad";
+        public static string CLIENT_ID = "<clientid>";
+        public static string CLIENT_SECRET = "<clientsecret>";
+        public static string REFRESH_TOKEN = "refresh_token"; // Replace with your actual refresh token or leave empty to prompt for it
         public static string ACCOUNT_SERVER_URL = "https://accounts.zoho.com";
     }
 }
